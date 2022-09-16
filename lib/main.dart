ea4a88562c7afc122fa6142793c66b54d155e4da
sulaiman-13 <sulaimanismaeel13@gmail.com>
2022-09-16 21:20:16 +0300

    counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bloc_example/bloc/counter_bloc.dart';
-import 'package:bloc_example/pages/home_page.dart';
+import 'bloc/counter_bloc.dart';
+import 'pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
